f904555425597f6db39c67069b7e1b5af49f9101
M'JID Yannis <M.JID.Yannis@gmail.com>
2016-09-25 18:01:02 +0200

    Suppression des fichiers de profils provenant du 1er commit

--- bdd/bdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$dbname="bdd_capprio";
-$username="root";
-$pwd="simplon";
-$bdd = new PDO('mysql:host='.$host.';dbname='.$dbname.'',''.$username.'',''.$pwd.'');
-$bdd->exec("SET CHARACTER SET utf8");
-
-?>
